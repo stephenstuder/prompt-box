47b84970bf8d91266b6445cb3eb7bbb2bedab7cd
stephen studer <stephenkstuder@gmail.com>
2020-04-28 15:46:32 -0700

    styles

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function ask(options) {
     return new Promise ((resolve, reject) => {
         // First we need to create a popup with all the fields in it
         const popup = document.createElement('form');
-        
+        popup.classList.add('popup');
         // Check if they want a cancel button 
 
         // Listen for a submit event on the inputs
